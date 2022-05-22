d929d76e04988d64c223e87453e30a3eb993f007
Alexandr Sanatar <allexvik@gmail.com>
2022-05-22 23:21:14 +0300

    v1.01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,17 @@ import { getContacts } from './Operations/operations';
 import { useEffect } from 'react';
 export default function App() {
   const dispatch = useDispatch();
-  async function testAsyncFunc() {
-    let x = await fetch('https://628a1a225da6ddfd5d5fbdb3.mockapi.io/contacts');
-    let xx = await x.json();
-    console.log('xx: ', xx);
-  }
+  // async function testAsyncFunc() {
+  //   let x = await fetch('https://628a1a225da6ddfd5d5fbdb3.mockapi.io/contacts');
+  //   let xx = await x.json();
+  //   console.log('xx: ', xx);
+  // }
   useEffect(() => {
     dispatch(getContacts());
   });
   return (
     <div>
-      <button onClick={testAsyncFunc}>GETAPIDATA</button>
+      {/* <button onClick={testAsyncFunc}>GETAPIDATA</button> */}
       <Phonebook />
       <TodoRender />
     </div>
